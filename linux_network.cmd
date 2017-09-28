20dd61258c6ec08f5b634d701f5ba88ab22fedf5
Ramesh D <plasmixs@users.noreply.github.com>
2017-09-28 15:13:09 +0530

    Update and rename general.cmd to linux_network.cmd
    Update linux networking commands for vlan and iptables

---
+++ linux_network.cmd
@@ -0,0 +1,32 @@
+Interfaces
+==========
+
+sudo vim /etc/network/interfaces
+auto eth1
+iface eth1 inet static
+        address 192.168.100.20
+        netmask 255.255.255.0
+
+sudo /etc/init.d/networking restart
+
+VLAN settings
+=============
+
+https://www.candelatech.com/~greear/vlan.html
+https://wiki.ubuntu.com/vlan
+
+MAC VLAN
+========
+
+Hostname
+========
+
+sudo hostnamectl set-hostname odl
+sudo vim /etc/hosts
+
+IPTables
+========
+
+#sudo iptables --policy FORWARD ACCEPT
+#sudo iptables -L
+
